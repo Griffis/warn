33435be09b383a4dd9d1e283f3e0e1074fd40e23
griffis <jalehreeskzqkw@yahoo.com>
2025-01-29 21:36:34 +0000

    Document UI - adding new feature

--- demo/src/typings.d.ts
+++ demo/src/typings.d.ts
@@ -1,8 +1,8 @@
 /* SystemJS module definition */
-declare let module: NodeModule;
+declare const module: NodeModule;
 interface NodeModule {
   id: string;
 }
 
-declare let System: any;
+declare const System: any;
 declare module "menuspy";
